16ff38bc5222dadf791594508bf4a9e0e83121d4
TheCoolestBerry <115311057+TheCoolestBerry@users.noreply.github.com>
2022-11-27 21:24:37 -0700

    Update p5.js

--- dev/p5.js
+++ dev/p5.js
@@ -11,146 +11,148 @@ function draw() {
   timelimit = 25;
   
   //wall variables
-  rect1X = 80;
-  rect1Y = 70;
-  rect1Length = 280;
-  rect1Width = 10;
+  i1= createDiv("");
+  i2 = createDiv("");
+  i3 = createDiv("");
+  i4 = createDiv("");
+  i5 = createDiv("");
+  i6 = createDiv("");
   
+  c = createDiv("");
+  
+  
+  i1.position(580,80);
+  i1.size(440,50);
+  
+  i2.position(1020,80);
+  i2.size(440,50);
+  
+  i3.position(1020,0);
+  i3.size(580,900);
+  
+  i4.position(450,640);
+  i4.size(800,260);
+  
+  i5.position(450,240);
+  i5.size(130,450);
+  
+  i6.position(450,240);
+  i6.size(450,70);
+  
+  c.position(970,540);
+  c.size(50,80);
+  
+  i1.mouseOver(tryagain);
+  i2.mouseOver(tryagain);
+  i3.mouseOver(tryagain);
+  i4.mouseOver(tryagain);
+  i5.mouseOver(tryagain);
+  i6.mouseOver(tryagain);
+  
+  c.mouseOver(goodjob);
   
   
   
   //horizontal walls pt1
   
   fill(20,140,140);
-  rect(80,70,280,10);
- if(circY >= 70 && circY <= 80){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(580,130,420,20);
+  
+  //placeholderbarriers
+  
+  fill(30,30,30);
+  rect(580,80,440,50);
+  i1.position(580,80);
+  i1.size(440,50);
+  
+  fill(90,90,90);
+  rect(1020,80,50,700);
+  i2.position(1020,80);
+  i2.size(50,700);
+  
+  fill(120,120,120);
+  rect(450,640,800,80);
+  i3.position(450,640);
+  i3.size(800,80);
+  
+  fill(150,150,150);
+  rect(510,240,70,450);
+  i4.position(510,240);
+  i4.size(70,450);
+  
+  fill(180,180,180);
+  rect(450,240,450,70);
+  i5.position(450,240);
+  i5.size(450,240);
+  
+  fill(210,210,210);
+  rect(695,410,350,110);
+  
+//starting point
  
- 
+ //wall
+  
   fill(20,140,140);
-  rect(80,130,230,10);
-if(circY >= 130 && circY <= 140){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
-  
-  //path
-  
-  fill(20,130)
-  rect(80,80,230,50);
+  rect(580,220,320,20);
+
+
   
   //vertical walls
   
   fill(20,140,140);
-  rect(360,70,10,180);
-if(circX >= 360 && circX <= 370){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(1000,130,20,290);
   
   
   fill(20,140,140);
-  rect(300,130,10,60);
-if(circY >= 70 && circY <= 80){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
-  
-  // path
-  
-  fill(20,130)
-  rect(310,80,50,160);
-
+  rect(900,220,20,80);
   
 //horizontal walls pt2
   
   fill(20,140,140);
-  rect(140,240,220,10);
-if(circX >= 240 && circX <= 250){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(600,300,320,20);
+
   
   fill(20,140,140);
-  rect(80,180,220,10);
-if(circY >= 280 && circY <= 290){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(690,400,310,20);
+
   
-  //path
-  
-  fill(20,130)
-  rect(80,190,230,50);
   
   
   //vertical walls pt2
   
   fill(20,140,140);
-  rect(70,180,10,170);
-  if(circY >= 180 && circY <= 190){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
-  
-  fill(20,140,140);
-  rect(130,240,10,50);
+  rect(580,300,20,340);
 
   
-  //correct path
-  
-  fill(20,130)
-  rect(80,190,50,150);
+  fill(20,140,140);
+  rect(690,420,20,100);
+
 
   
   
   //walls pt3
   fill(20,140,140);
-  rect(80,340,280,10);
-if(circY >= 340 && circY <= 350){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(690,520,330,20);
+
   
   fill(20,140,140);
-  rect(140,280,220,10);
-if(circY >= 280 && circY <= 290){
-    fill(255);
-    stroke(0);
-    textSize(24);
-    text('Haha loser!!',80,60) 
-    }
+  rect(600,620,420,20);
+
   
-  //path
-  
-  fill(20,130)
-  rect(80,290,270,50);
+  fill(20,140,140);
+  rect(1020,520,20,120);
+
   
   
-  
+  //finish line
   fill(200,140,170);
-  rect(330,290,30,50);
+  rect(970,540,50,80);
+  c.position(970,540);
+  c.size(50,80);
   
+  //mouse
   fill(255,20,30);
-  ellipse(circX,circY,25,25);
+  mouse = ellipse(circX,circY,50,50);
   
   //timer
     totaltime = int(mil/1000);
@@ -167,33 +169,22 @@ if(circY >= 280 && circY <= 290){
   
   d = int(dist(circX, circY, 330, 300));
   
-///win screen 
-  if(d<30){
-     fill(255);
-     stroke(0);
-     textSize(20);
-     text('Congratulations, much proud !!',80,60);
-    
+//tryagain
+
+function tryagain(){
+  fill(255);
+  stroke(0);
+  textSize(54);
+  text('Haha Loser!',1000,80);
+}
   
-  //lose screen
-    function lose(){
-     fill(255);
-     stroke(0);
-     textSize(44);
-     text('Haha loser!!',130,30) 
-    }
-    
-    if(gametime <= 0){
-     fill(255);
-     stroke(0);
-     textSize(44);
-     text('Haha loser!!',130,30)
-    }
-    
+function goodjob(){
+  fill(255);
+  stroke(0);
+  textSize(54);
+  text('LESGOOOOO',1000,80);
+}
   
-  //try again
-   
   
-  }
   
 }
