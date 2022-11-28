152e23d0e9ded953c37cdb7e2872158ac15ee2fd
Ulfrid9639 <119280900+Ulfrid9639@users.noreply.github.com>
2022-11-27 22:45:09 -0700

    Update color.js
    Final coloring; Find 7 invisible circles around the canvas via click and dragging/touch (=coloring). When all 7 are discovered, the users can manually press the 'If you find all 7 circles, click to restart' button on the top left corner to restart.

--- dev/color.js
+++ dev/color.js
@@ -2,12 +2,54 @@ function setup() {
   createCanvas(1600, 900);
   background(220);
 }
+
 function draw() {
 
   noStroke();
   fill(220);
-circle(800,450,300);
+circle(50,400,80);
+ 
+    noStroke();
+  fill(220);
+circle(1555,100,30);
+ 
+  noStroke();
+  fill(220);
+circle(350,480,30);
+ 
+  noStroke();
+  fill(220);
+circle(700,800,150);
+ 
+  noStroke();
+  fill(220);
+circle(800,600,50);
+ 
+  noStroke();
+  fill(220);
+circle(1300,380,100);
+ 
+  noStroke();
+  fill(220);
+circle(1000,380,150);
+ 
+   button = createButton('If you find all 7 circles, click to restart');
+        button.position(1, 1);
+        button.size(150, 100);
+        button.mousePressed(res);
 
+
+   fill(0);
+
+
+
+ 
+function res(){
+    location.reload();
+
+}
+
+ 
   if(mouseIsPressed) {
     fill(123,63,0);
   }
